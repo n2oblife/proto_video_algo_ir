2c21c68effa11fb5d67b62e311ac9eae8cbd4c97
n2oblife <zackanit@gmail.com>
2024-05-23 17:24:39 +0200

    build project struct

---
+++ algorithms/AdaSBNUCIRFPA.py
@@ -0,0 +1,26 @@
+# Adaptive scene-based non-uniformity correction method for infrared focal plane arrays
+# AdaSBNUCIRFPA
+
+def SBNUCIRFPA():
+    return
+
+def SBNUCIRFPA_update_g():
+    return 
+
+def SBNUCIRFPA_update_o():
+    return 
+
+def SBNUCIRFPA_update_():
+    return 
+
+def AdaSBNUCIRFPA():
+    return
+
+def AdaSBNUCIRFPA_update_g():
+    return 
+
+def AdaSBNUCIRFPA_update_o():
+    return 
+
+def AdaSBNUCIRFPA_update_():
+    return 
---
+++ main.py
@@ -0,0 +1,23 @@
+def init():
+    return
+
+def update():
+    return
+
+def estimate():
+    return
+
+def compute_error():
+    return
+
+def metrics():
+    return
+
+def main():
+    init()
+    for _ in range():
+        update()
+        estimate()
+        compute_error()
+        metrics()
+    
